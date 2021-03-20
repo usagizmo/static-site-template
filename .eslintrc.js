32ab247b8018c13e3b2d265bad236d0c59079b1a
usagizmo <usagizmo@gmail.com>
2021-03-20 21:35:12 +0900

    Add husky, eslint, stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    jquery: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+}
---
+++ .stylelintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  rules: {
+    'prettier/prettier': true,
+    'block-no-empty': null,
+  },
+}
